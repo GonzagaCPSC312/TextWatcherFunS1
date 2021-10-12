ffb0a3e32a702a64b976f32fe68d2477f7579c53
Gina Sprint <sprint@gonzaga.edu>
2021-10-12 11:11:48 -0700

    finsih

--- app/src/main/java/com/sprint/gina/textwatcherfuns1/MainActivity.java
+++ app/src/main/java/com/sprint/gina/textwatcherfuns1/MainActivity.java
@@ -3,6 +3,8 @@ package com.sprint.gina.textwatcherfuns1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,5 +38,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // we don't need the hello button
+        // we can listen for text changed events
+        // with the TextWatcher interface
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // we can use this one to get the name the user types
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // we can also use this one to get the name the user types
+                String name = s.toString();
+                if (name.length() > 0) {
+                    helloTextView.setText("Hello, " + name);
+                }
+                else {
+                    helloTextView.setText("");
+                }
+            }
+        });
     }
 }
